ca60f4ec1c32e02762a60f043415ead9bc1003ef
mawippel <mawippel2@hotmail.com>
2019-03-29 20:36:44 -0300

    refactor: moving the code to the right class.

--- src/main/java/com/furb/br/Node.java
+++ src/main/java/com/furb/br/Node.java
@@ -65,7 +65,7 @@ public class Node {
 	private void startsElection() {
 		var newCoordinator = findNewCoordinator();
 		electionManagerInstance.setCoordinator(newCoordinator);
-		System.out.println(String.format("[%s] Processo de Eleição finalizado. O novo coordenador é %s.",
+		System.out.println(String.format("[%s] Processo de Eleiï¿½ï¿½o finalizado. O novo coordenador ï¿½ %s.",
 				LocalDateTime.now(), newCoordinator));
 	}
 
@@ -76,7 +76,7 @@ public class Node {
 	 * @return a {@link NodeCoordinator}
 	 */
 	private NodeCoordinator findNewCoordinator() {
-		System.out.println(String.format("[%s] Processo de Eleição iniciado pelo %s.", LocalDateTime.now(), this));
+		System.out.println(String.format("[%s] Processo de Eleiï¿½ï¿½o iniciado pelo %s.", LocalDateTime.now(), this));
 		electionManagerInstance.setInElection(true);
 		return getCoordinator(new NodeCoordinator(this));
 	}
@@ -88,37 +88,12 @@ public class Node {
 	private void consumeResource() {
 		System.out.println(String.format("[%s] Processo %s solicitou consumir um recurso.", LocalDateTime.now(), this));
 		if (!electionManagerInstance.isUsingResource()) {
-			lockResourceInNewThread();
+			electionManagerInstance.getCoordinator().lockResourceInNewThread(this);
 		} else {
 			// If there's some process consuming, add it to the Queue
 			electionManagerInstance.getCoordinator().getQueue().add(this);
 			System.out.println(String.format("[%s] Node %s foi adicionado a fila.", LocalDateTime.now(), this));
 		}
-	}
-
-	/**
-	 * Locks the resource in a new Thread for a random time, between 5-15 secs.
-	 * After unlocking the resource, it looks up to the Queue and continues it.
-	 */
-	private void lockResourceInNewThread() {
-		new Thread(() -> {
-			electionManagerInstance.startUsingResource(this);
-			try {
-				Thread.sleep(ThreadLocalRandom.current().nextInt(AppConstants.LOCK_RESOURCE_INIT,
-						AppConstants.LOCK_RESOURCE_LIMIT));
-			} catch (InterruptedException e) {
-				e.printStackTrace();
-			}
-			electionManagerInstance.stopUsingResource(this);
-
-			NodeCoordinator coordinator = electionManagerInstance.getCoordinator();
-			if (coordinator != null) {
-				Node next = coordinator.getQueue().poll();
-				if (next != null) {
-					next.lockResourceInNewThread();
-				}
-			}
-		}).run();
 	}
 
 	private void scheduleNextExecution() {
--- src/main/java/com/furb/br/NodeCoordinator.java
+++ src/main/java/com/furb/br/NodeCoordinator.java
@@ -2,17 +2,44 @@ package com.furb.br;
 
 import java.util.LinkedList;
 import java.util.Queue;
+import java.util.concurrent.ThreadLocalRandom;
 
 import lombok.Data;
 
 @Data
 public class NodeCoordinator {
 
+	private final ElectionManager electionManagerInstance = ElectionManager.getInstance();
 	private Queue<Node> queue = new LinkedList<>();
 	private Node node;
 	
 	public NodeCoordinator(Node node) {
 		this.node = node;
+	}
+	
+	/**
+	 * Locks the resource in a new Thread for a random time, between 5-15 secs.
+	 * After unlocking the resource, it looks up to the Queue and continues it.
+	 */
+	public void lockResourceInNewThread(Node n) {
+		new Thread(() -> {
+			electionManagerInstance.startUsingResource(n);
+			try {
+				Thread.sleep(ThreadLocalRandom.current().nextInt(AppConstants.LOCK_RESOURCE_INIT,
+						AppConstants.LOCK_RESOURCE_LIMIT));
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+			electionManagerInstance.stopUsingResource(n);
+
+			NodeCoordinator coordinator = electionManagerInstance.getCoordinator();
+			if (coordinator != null) {
+				Node next = coordinator.getQueue().poll();
+				if (next != null) {
+					lockResourceInNewThread(next);
+				}
+			}
+		}).run();
 	}
 
 }
